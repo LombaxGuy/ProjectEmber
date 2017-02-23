abbf98b17c8257ad3a5789986453473636a97b3f
Lasse Olskær <lolskaer@yahoo.dk>
2017-02-23 11:39:59 +0100

    Changed: Changed zoom from FoV to camera.z

--- Assets/Scripts/Zoom.cs
+++ Assets/Scripts/Zoom.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Zoom : MonoBehaviour {
+public class Zoom : MonoBehaviour
+{
 
-    private float zoomSpeed = 0.2f;
+    [SerializeField]
+    private float maxZoomSpeed = 0.5f;
+    [SerializeField]
+    private float hardMinCap = -10;
+    [SerializeField]
+    private float hardMaxCap = -15;
+
+    private float minZoomSpeed = 0.05f;
 
     // Use this for initialization
+    void Start()
+    {
+
+    }
+
     void Update()
     {
         if (Input.touchCount == 2)
@@ -29,11 +42,18 @@ public class Zoom : MonoBehaviour {
 
         //Difference mellem de to udregnede distancer
         float deltaDifference = previousTouchDistance - curTouchDistance;
+        Debug.Log(deltaDifference);
 
-        
-        this.GetComponent<Camera>().fieldOfView += deltaDifference * zoomSpeed;
+        float zoomSpeed = (maxZoomSpeed / 100) * (touch1.position.y - touch0.position.y);
+        zoomSpeed = Mathf.Clamp(zoomSpeed, minZoomSpeed, maxZoomSpeed);
 
-        this.GetComponent<Camera>().fieldOfView = Mathf.Clamp(this.GetComponent<Camera>().fieldOfView, 40f, 90f);
+        if (deltaDifference > 0 && this.transform.position.z < -10 || deltaDifference < 0 && this.transform.position.z > -15)
+        {
+            this.transform.position += new Vector3(0, 0, deltaDifference * zoomSpeed);
+        }
+
+
 
     }
+
 }
