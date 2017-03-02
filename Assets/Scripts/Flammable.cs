7c79cba73d4ff9ef6bdf5f97beea3cd92b22ee43
Thorbjorn <superthorbjorn@gmail.com>
2017-03-02 10:16:56 +0100

    Projectile

---
+++ Assets/Scripts/Flammable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flammable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.tag == "Projectile")
+        {
+            other.gameObject.GetComponent<ProjectileLife>().FirePos = gameObject.transform.GetChild(0).transform.position;
+            other.gameObject.GetComponent<ProjectileLife>().Fizzle(true, 1);
+        }
+
+
+    }
+}
