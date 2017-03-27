2e18450af071b800091f07d9d69083243e48815d
Mikkel Sommer Kristensen <mikkelsk64@hotmail.com>
2017-03-27 08:53:59 +0200

    Map selection changes

--- Assets/Scripts/MapSelectionManager.cs
+++ Assets/Scripts/MapSelectionManager.cs
@@ -35,22 +35,27 @@ public class MapSelectionManager : MonoBehaviour
     private int wellSelected;
     [Tooltip("Number of wells in the scene")]
     [SerializeField]
-    private int maxWells;
+    private int maxWells = 3;
 
+    
 
     #region Swipe
     [SerializeField]
-    private GameObject scrollWell;
+    private GameObject swipeWell;
 
     private float margin = 200;
 
     private float xMaxSoft = 0;
-    private float xMinSoft = -1600;
+    private float xMinSoft;
+    private float yMaxSoft = 0;
+    private float yMinSoft;
 
     private float xMinHard;
     private float xMaxHard;
+    private float yMinHard;
+    private float yMaxHard;
 
-    private float xDynamicSpeed = 1;
+    private float dynamicSpeed = 1;
 
     private Vector3 oldMousePos;
 
@@ -60,13 +65,17 @@ public class MapSelectionManager : MonoBehaviour
 
     private float horizontalMoveSpeed = 0.5f;
 
-    private float wellSnapSize = 800;
+    private float wellSpacing = 800;
+
+    Coroutine centeringCoroutine;
     #endregion
 
     // Use this for initialization
     void Start()
     {
         #region Swipe
+        xMinSoft = -1 * (maxWells - 1) * wellSpacing;
+
         xMinHard = xMinSoft - margin;
         xMaxHard = xMaxSoft + margin;
 
@@ -83,10 +92,9 @@ public class MapSelectionManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (inWell == true)
+        if (inWell)
         {
             HandleSwipeHorizontal();
-            //SwipeLeftOrRight();
         }
         else
         {
@@ -101,6 +109,11 @@ public class MapSelectionManager : MonoBehaviour
 
         if (Input.GetMouseButtonDown(0))
         {
+            if (centeringCoroutine != null)
+            {
+                StopCoroutine(centeringCoroutine);
+            }
+   
             oldMousePos = Input.mousePosition;
             Debug.Log("DOWN: " + oldMousePos);
         }
@@ -108,67 +121,147 @@ public class MapSelectionManager : MonoBehaviour
         {
             deltaPosX = Input.mousePosition.x - oldMousePos.x;
 
-            
             // If the camera is located within the x-bounds of the map...
-            if (scrollWell.transform.localPosition.x > xMinSoft && scrollWell.transform.localPosition.x < xMaxSoft)
+            if (swipeWell.transform.localPosition.x > xMinSoft && swipeWell.transform.localPosition.x < xMaxSoft)
             {
                 //... the dynamic speed on the x-axis is set to 1. 
-                xDynamicSpeed = 1;
+                dynamicSpeed = 1;
             }
             // If the camera is out of bounds on the left(-x) side of the map... 
-            else if (scrollWell.transform.localPosition.x < xMinSoft)
+            else if (swipeWell.transform.localPosition.x < xMinSoft)
             {
                 //xDynamicSpeed = 0;
                 //... calculate the dynamic speed on the x-axis.
-                CalculateDynamicSpeed(xMinSoft, xMinHard, ref xDynamicSpeed);
+                CalculateDynamicSpeed(xMinSoft, xMinHard, swipeWell.transform.localPosition.x, ref dynamicSpeed);
             }
             // If the camera is out of bounds on the right(+x) side of the map... 
-            else if (scrollWell.transform.localPosition.x > xMaxSoft)
+            else if (swipeWell.transform.localPosition.x > xMaxSoft)
             {
                 //xDynamicSpeed = 0;
                 //... calculate the dynamic speed on the x-axis.
-                CalculateDynamicSpeed(xMaxSoft, xMaxHard, ref xDynamicSpeed);
+                CalculateDynamicSpeed(xMaxSoft, xMaxHard, swipeWell.transform.localPosition.x, ref dynamicSpeed);
             }
 
-            Debug.Log("MOVED: " + Input.mousePosition + " . " + oldMousePos + " . " + xDynamicSpeed);
+            Debug.Log("MOVED: " + Input.mousePosition + " . " + oldMousePos + " . " + dynamicSpeed);
 
-            scrollWell.transform.Translate(deltaPosX * horizontalMoveSpeed * xDynamicSpeed, 0, 0, Space.Self);
+            swipeWell.transform.Translate(deltaPosX * horizontalMoveSpeed * dynamicSpeed, 0, 0, Space.Self);
 
             Vector3 pos;
 
-            pos.x = Mathf.Clamp(scrollWell.transform.localPosition.x, xMinHard, xMaxHard);
-            pos.y = scrollWell.transform.localPosition.y;
-            pos.z = scrollWell.transform.localPosition.z;
+            pos.x = Mathf.Clamp(swipeWell.transform.localPosition.x, xMinHard, xMaxHard);
+            pos.y = swipeWell.transform.localPosition.y;
+            pos.z = swipeWell.transform.localPosition.z;
 
-            oldMousePos = Input.mousePosition;
+            swipeWell.transform.localPosition = pos;
 
-            
+            oldMousePos = Input.mousePosition; 
         }
         else if (Input.GetMouseButtonUp(0))
         {
-            // Round to neareset step *** HARD SNAP ATM ***
-            Debug.Log(Mathf.Round(scrollWell.transform.localPosition.x / wellSnapSize));
+            Vector3 fromPos = swipeWell.transform.localPosition;
+            Vector3 toPos = new Vector3(Mathf.Round(swipeWell.transform.localPosition.x / wellSpacing) * wellSpacing, swipeWell.transform.localPosition.y, swipeWell.transform.localPosition.z);
 
-            scrollWell.transform.localPosition = new Vector3(Mathf.Round(scrollWell.transform.localPosition.x / wellSnapSize) * wellSnapSize, scrollWell.transform.localPosition.y, scrollWell.transform.localPosition.z);
+            centeringCoroutine = StartCoroutine(CoroutineSnapToPosition(fromPos, toPos, 0.25f));
 
-            xDynamicSpeed = 1; 
+            dynamicSpeed = 1; 
+        }
+#endif
+    }
+
+    private void HandleSwipeVertical()
+    {
+#if(DEBUG)
+        float deltaPosY = 0;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (centeringCoroutine != null)
+            {
+                StopCoroutine(centeringCoroutine);
+            }
+
+            oldMousePos = Input.mousePosition;
+            Debug.Log("DOWN: " + oldMousePos);
+        }
+        else if (Input.GetMouseButton(0) && oldMousePos != Input.mousePosition)
+        {
+            deltaPosY = Input.mousePosition.y - oldMousePos.y;
+
+            // If the camera is located within the x-bounds of the map...
+            if (swipeWell.transform.localPosition.y > xMinSoft && swipeWell.transform.localPosition.y < xMaxSoft)
+            {
+                //... the dynamic speed on the x-axis is set to 1. 
+                dynamicSpeed = 1;
+            }
+            // If the camera is out of bounds on the left(-x) side of the map... 
+            else if (swipeWell.transform.localPosition.y < xMinSoft)
+            {
+                //xDynamicSpeed = 0;
+                //... calculate the dynamic speed on the x-axis.
+                CalculateDynamicSpeed(xMinSoft, xMinHard, swipeWell.transform.localPosition.y, ref dynamicSpeed);
+            }
+            // If the camera is out of bounds on the right(+x) side of the map... 
+            else if (swipeWell.transform.localPosition.y > xMaxSoft)
+            {
+                //xDynamicSpeed = 0;
+                //... calculate the dynamic speed on the x-axis.
+                CalculateDynamicSpeed(xMaxSoft, xMaxHard, swipeWell.transform.localPosition.y, ref dynamicSpeed);
+            }
+
+            Debug.Log("MOVED: " + Input.mousePosition + " . " + oldMousePos + " . " + dynamicSpeed);
+
+            swipeWell.transform.Translate(0, deltaPosY * horizontalMoveSpeed * dynamicSpeed, 0, Space.Self);
+
+            Vector3 pos;
+
+            pos.x = swipeWell.transform.localPosition.x;
+            pos.y = Mathf.Clamp(swipeWell.transform.localPosition.x, xMinHard, xMaxHard);
+            pos.z = swipeWell.transform.localPosition.z;
+
+            swipeWell.transform.localPosition = pos;
+
+            oldMousePos = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            Vector3 fromPos = swipeWell.transform.localPosition;
+            Vector3 toPos = new Vector3(swipeWell.transform.localPosition.x, Mathf.Round(swipeWell.transform.localPosition.y / wellSpacing) * wellSpacing, swipeWell.transform.localPosition.z);
+
+            centeringCoroutine = StartCoroutine(CoroutineSnapToPosition(fromPos, toPos, 0.25f));
+
+            dynamicSpeed = 1;
         }
 #endif
     }
 
     /// <summary>
-    /// Used to calculate the dynamic movement speed of the camera.
+    /// Used to calculate the dynamic movement speed of the UI elements.
     /// </summary>
     /// <param name="softCap">The soft cap used in the calculation.</param>
     /// <param name="hardCap">The hard cap used in the calculation.</param>
     /// <param name="dynamicSpeed">The speed variable that should be used in the calculation. !!NOTE!! This value is changed during the calculation.</param>
-    private void CalculateDynamicSpeed(float softCap, float hardCap, ref float dynamicSpeed)
+    private void CalculateDynamicSpeed(float softCap, float hardCap, float currentAxisPos, ref float dynamicSpeed)
     {
         float percent = 0;
 
-        percent = 1 - (softCap - scrollWell.transform.localPosition.x) / /*Mathf.Abs*/(softCap - hardCap);
+        percent = 1 - (softCap - currentAxisPos) / (softCap - hardCap);
         dynamicSpeed = Mathf.Pow(percent, 2);
     }
+
+    private IEnumerator CoroutineSnapToPosition(Vector3 fromPos, Vector3 toPos, float snapTime)
+    {
+        float t = 0;
+
+        while (t < 1)
+        {
+            t += Time.deltaTime / snapTime;
+
+            swipeWell.transform.localPosition = Vector3.Lerp(fromPos, toPos, t);
+
+            yield return null;
+        }
+    }
+
 
     //Swipe left or right for picking wells
     private void SwipeLeftOrRight()
@@ -217,6 +310,7 @@ public class MapSelectionManager : MonoBehaviour
             }
         }
     }
+
     //Swipe up or down for picking levels inside wells
     private void SwipeUpOrDown()
     {
