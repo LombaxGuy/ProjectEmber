42fdedaced8ff859c0adeddf994936a3641ff35f
Mathias Jensen <mathias_2010@live.dk>
2017-04-04 14:22:56 +0200

    Powerup Framework

---
+++ Assets/Scripts/Powerups/Powerup.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Powerup : MonoBehaviour {
+
+    public enum Powerups { PowerShot, SeeWhereTheFlameGoes, OpBounce, MoreLight }
+
+    private int turns;
+    private Powerups powerUps;
+    //Variabel med det forskellige objecter der skal ændres på
+
+    public int Turns
+    {
+        get { return turns; }
+        set { turns = value; }
+    }
+
+    public Powerups PowerUps
+    {
+        get { return powerUps; }
+        set { powerUps = value; }
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+   
+
+    public abstract void UsePowerup();
+
+    public abstract void NextTurn();
+
+    public abstract void ResetValues();
+
+
+}
