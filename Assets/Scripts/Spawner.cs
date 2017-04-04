ec5a6ca7e1ff38142245c443205efa73be0f0441
Thorbjorn <superthorbjorn@gmail.com>
2017-04-04 10:37:59 +0200

    Comented and cleaned scripts

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -7,26 +7,18 @@ public class Spawner : MonoBehaviour {
     [SerializeField]
     private GameObject spawnMark;
     [SerializeField]
-    private GameObject targetMark;
-
-    [SerializeField]
     private Vector3 createPos;
-    [SerializeField]
-    private Vector3 targetPos;
-    [SerializeField]
-    private Vector3 dir;
-
     [SerializeField]
     private GameObject[] projectiles;
 
     //Settings
-    private float launchSpeed;
     [Header("Settings")]
     [SerializeField]
     private int p_MaxP;
     [SerializeField]
     private float interval;
 
+    //Can be used to turn spawning on and off if that is needed in future map creation
     private bool spawnerOn = false;
 
     [SerializeField]
@@ -40,8 +32,6 @@ public class Spawner : MonoBehaviour {
 	void Start () {
 
         createPos = spawnMark.transform.position;
-        targetPos = targetMark.transform.position;
-        dir = createPos + targetPos;
         projectiles = new GameObject[p_MaxP];
         for (int i = 0; i < projectiles.Length; i++)
         {
@@ -50,17 +40,6 @@ public class Spawner : MonoBehaviour {
         }
         Launch();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-
-        //if (Input.GetKeyUp(KeyCode.G))
-        //{
-        //    spawnerOn = !spawnerOn;
-        //    Launch();
-            
-        //}        
-    }
 
     /// <summary>
     /// USed to launch the coroutine
@@ -71,21 +50,6 @@ public class Spawner : MonoBehaviour {
         StartCoroutine("Drip");       
     }
 
-    /// <summary>
-    /// This is for if we need to fling water in a direction
-    /// </summary>
-    /// <returns></returns>
-    private IEnumerator move()
-    {
-        float t = 0;
-        Debug.Log("called");
-        while (t < 1)
-        {
-            t += Time.deltaTime / 2;
-            projectiles[0].GetComponent<Rigidbody>().AddForce(dir.normalized, ForceMode.Acceleration);
-            yield return null;
-        }
-    }
 
     /// <summary>
     /// Handles the spawning of the projectiles
