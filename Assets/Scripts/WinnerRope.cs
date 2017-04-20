6edc9cb01feb7b33dc900454bf59c621e2d2bc95
Thorbjorn <superthorbjorn@gmail.com>
2017-04-20 10:42:24 +0200

    jvih

---
+++ Assets/Scripts/WinnerRope.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinnerRope : MonoBehaviour
+{
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Projectile")
+        {
+            //Skift eller noget
+            //Lav en fucking animation yo
+        }
+    }
+}
