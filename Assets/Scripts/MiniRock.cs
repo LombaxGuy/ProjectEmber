03fb703aee015aa91fb2b7b7c822b6c62d43ead7
Lasse Olskær <lolskaer@yahoo.dk>
2017-05-10 18:25:00 +0200

    Added Rock interactable

---
+++ Assets/Scripts/MiniRock.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniRock : MonoBehaviour
+{
+
+    [SerializeField]
+    private const float LIFETIME = 2f;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    /// <summary>
+    /// Used to start the timer coroutine when the droplet is activated.
+    /// </summary>
+    private void OnDisable()
+    {
+        //if (rb != null)
+        //{
+            rb.velocity = Vector3.zero;
+        //}
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Environment"))
+        {
+            StartCoroutine(CoroutineTimer());
+        }
+    }
+
+    /// <summary>
+    /// Coroutine that waits for a set amount of time and then disables the droplet.
+    /// </summary>
+    /// <param name="time">Time in seconds. Set this to use another time than the lifeTime.</param>
+    private IEnumerator CoroutineTimer(float time = LIFETIME)
+    {
+        // Waits for a specified amount of time.
+        yield return new WaitForSeconds(time);
+
+        // Disables the droplet gameobject.
+        gameObject.SetActive(false);
+    }
+
+    public void Explosion(float explosionForce, Vector3 explosionPoint)
+    {
+        rb.AddExplosionForce(explosionForce, explosionPoint, 2f);
+    }
+
+}
