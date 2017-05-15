bffa4e20074160f06beb58a19b437ec23800bf6f
Lasse Olskær <lolskaer@yahoo.dk>
2017-05-15 12:20:53 +0200

    Fixed rocks and added gates

--- Assets/Scripts/Gate.cs
+++ Assets/Scripts/Gate.cs
@@ -2,28 +2,96 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Gate : MonoBehaviour {
+public class Gate : MonoBehaviour
+{
 
+    private HingeJoint hinge;
+    private JointLimits limits;
 
+    private IEnumerator corutine;
 
-	// Use this for initialization
-	void Start () {
-        
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    private bool closeGate = true;
+
+    [SerializeField]
+    private float closedWaitTimer = 3f;
+    [SerializeField]
+    private float openWaitTimer = 3f;
+
+    [SerializeField]
+    private bool closeAtStart = false;
+
+    private bool isClosing = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        corutine = GateTimer(closedWaitTimer, openWaitTimer);
+
+        hinge = GetComponent<HingeJoint>();
+        limits = hinge.limits;
+        if (!closeAtStart)
+        {
+        limits.bounciness = 0;
+        limits.bounceMinVelocity = 0;
+        limits.min = 0;
+        limits.max = -90;
+        hinge.limits = limits;
+        hinge.useLimits = true;
+            StartCoroutine(corutine);
+        }
+        else
+        {
+            limits.bounciness = 0;
+            limits.bounceMinVelocity = 0;
+            limits.min = 0;
+            hinge.limits = limits;
+            hinge.useLimits = true;
+        }
 
     }
 
-  private IEnumerator CloseGates()
+    // Update is called once per frame
+    void Update()
     {
-        float t = 0;
+       if(closeAtStart)
+        {
+            if(Input.GetKeyDown(KeyCode.Space))
+            {
+                OpenGate();
+            }
+        }
+    }
+
+    private IEnumerator GateTimer(float closedwaitTimer, float openWaitTimer)
+    {
+        Debug.Log("Blargh");
         while (true)
         {
-            t += Time.deltaTime / 4f;
+            if (!hinge.useMotor)
+            {
+                hinge.useMotor = true;
+                isClosing = true;
+            }
+            else
+            {
+                hinge.useMotor = false;
+                isClosing = false;
+            }
+            if (isClosing)
+            {
+                yield return new WaitForSeconds(closedWaitTimer);
+            }
+            else
+            {
+                yield return new WaitForSeconds(openWaitTimer);
+            }
 
-        }  
+        }
 
+    }
+
+    public void OpenGate()
+    {
+        hinge.useLimits = false;
     }
 }
---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject gate;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.transform.tag == "Projectile")
+        {
+            for (int i = 0; i < gate.transform.childCount; i++)
+            {
+                if(gate.transform.GetChild(i).transform.name.Contains("Door"))
+                {
+                    gate.transform.GetChild(i).GetComponent<Gate>().OpenGate();
+
+                }
+            }
+        }
+    }
+}
